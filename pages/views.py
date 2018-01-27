bf3e0c584d450a182390422907dc20624b4953cb
Matt Cole <matt@mattkcole.com>
2018-01-27 15:31:31 -0500

    updating with content

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# pages/views.py
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("hello world")
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
